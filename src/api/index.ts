363abbf37ce86a714bbcfbead9c19a7485db0f6a
孙本强 <sunbenqiang@msn.com>
2020-05-20 19:51:31 +0800

    update publicPath

--- src/api/index.ts
+++ src/api/index.ts
@@ -8,7 +8,7 @@ export default {
      * 获取data.json5内容
      */
     init(cb: (d: UserData) => void): void {
-        axios.get<string>('/data.json5', {
+        axios.get<string>('./data.json5', {
             params: {
                 // 为了解决修改完配置后无法立即生效的问题
                 version: (new Date()).getTime(),
